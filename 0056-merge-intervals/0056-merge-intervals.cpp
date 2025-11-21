817f14ca6c81026b447beba8b34be016e0d42889
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2025-11-22 03:00:55 +0600

    Time: 4 ms (68.62%), Space: 24 MB (34.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,29 +4,37 @@ vector<vector<int>> merge(vector<vector<int>> &intervals)
 {
     if (intervals.empty())
         return {};
-    vector<vector<int>> matrix;
     sort(intervals.begin(), intervals.end());
-    int x = intervals[0][0];
-    int y = intervals[0][1];
-    int j = 0;
+    vector<vector<int>> matrix;
+    matrix.push_back(intervals[0]);
+    // int x = intervals[0][0];
+    // int y = intervals[0][1];
+    // int j = 0;
     for (int i = 1; i < intervals.size(); i++)
     {
-        if (intervals[i][0] <= y)
-        {
-            y = max(y, intervals[i][1]);
-        }
-        else
-        {
-            // matrix[j][0] = x;
-            // matrix[j][1] = y;
-            matrix.push_back({x, y});
-            // j++;
+        // if (intervals[i][0] <= y)
+        // {
+        //     y = max(y, intervals[i][1]);
+        // }
+        // else
+        // {
+        //     // matrix[j][0] = x;
+        //     // matrix[j][1] = y;
+        //     matrix.push_back({x, y});
+        //     // j++;
 
-            x = intervals[i][0];
-            y = intervals[i][1];
-        }
+        //     x = intervals[i][0];
+        //     y = intervals[i][1];
+        // }
+        vector<int>& last = matrix.back();
+        if (intervals[i][0] <= last[1]) {
+                last[1] = max(last[1], intervals[i][1]);
+            } 
+            else {
+                matrix.push_back(intervals[i]);
+            }
     }
-    matrix.push_back({x, y});
+    // matrix.push_back({x, y});
     // matrix[i][0] = x;
     // matrix[i][1] = y;
     return matrix;
