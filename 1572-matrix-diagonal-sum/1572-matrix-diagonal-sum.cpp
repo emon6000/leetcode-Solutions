f8174f109337a604b1f79101943f5a03d6cae17a
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2025-11-22 13:48:55 +0600

    Time: 0 ms (100%), Space: 15.1 MB (16.47%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-      int sum=0;
-      int l = mat.size();
-      for(int i=0;i<l;i++){
-        sum+=mat[i][i];
-        int lg=mat[i].size();
-        if(i!=(lg-i-1)){
-        sum+=mat[i][lg-i-1];
+        int n=mat.size();
+        int sum=0;
+        for(int i =0; i<n;i++){
+            sum+= mat[i][i]+ mat[i][n-i-1];
         }
-      }
-      return sum;  
+        if(n%2==0){
+            return sum;
+        }
+        else{
+            return sum -= mat[n/2][n/2];
+        }
+    //   int sum=0;
+    //   int l = mat.size();
+    //   for(int i=0;i<l;i++){
+    //     sum+=mat[i][i];
+    //     int lg=mat[i].size();
+    //     if(i!=(lg-i-1)){
+    //     sum+=mat[i][lg-i-1];
+    //     }
+    //   }
+    //   return sum;  
     }
 };
