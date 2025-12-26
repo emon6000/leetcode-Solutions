a15fb52a028b586ce0d7bb5b396676615abd7d0c
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2025-12-26 23:21:23 +0600

    Time: 1 ms (57.81%), Space: 10.8 MB (21.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int l =0,r =s.size() -1;
+        while(l<r){
+            if(s[l]!=s[r]){
+                return false;
+            }
+            else{
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+};
