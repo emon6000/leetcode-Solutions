22d15b905fa1fbf28433d19b5e5580ff2a0e6f04
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-01-12 00:36:50 +0600

    Time: 4 ms (23.24%), Space: 12.7 MB (73.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int l = nums2.size();
-        vector<int>v;
-        map<int,int>mp;
-        bool f=false;
-        for(int i=0;i<l;i++){
-            for(int j=i+1;j<l;j++){
-                if(nums2[i]<nums2[j]){
-                    mp[nums2[i]]=nums2[j];
-                    f=true;
-                    break;
-                }
+        unordered_map<int,int>mp;
+        stack<int>st;
+        for(int i=0;i<nums2.size();i++){
+            while(!st.empty()&&st.top()<nums2[i]){
+                mp[st.top()]=nums2[i];
+                st.pop();
             }
-            if(!f){
-                mp[nums2[i]]=-1;
-            }
-            f=false;
+            st.push(nums2[i]);
         }
-
+        vector<int>ans;
         for(int i=0;i<nums1.size();i++){
-            v.push_back(mp[nums1[i]]);
+            if(mp.find(nums1[i])==mp.end()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(mp[nums1[i]]);
+            }
         }
+        return ans;
+        // int l = nums2.size();
+        // vector<int>v;
+        // map<int,int>mp;
+        // bool f=false;
+        // for(int i=0;i<l;i++){
+        //     for(int j=i+1;j<l;j++){
+        //         if(nums2[i]<nums2[j]){
+        //             mp[nums2[i]]=nums2[j];
+        //             f=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!f){
+        //         mp[nums2[i]]=-1;
+        //     }
+        //     f=false;
+        // }
 
-        return v;
+        // for(int i=0;i<nums1.size();i++){
+        //     v.push_back(mp[nums1[i]]);
+        // }
+
+        // return v;
     }
 };
