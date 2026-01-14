1df09ec68da7d7efb3e437e73908f554bb56e605
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-01-14 23:43:02 +0600

    Time: 0 ms (100%), Space: 9.7 MB (73.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
+        if(s.size() != t.size()) return false;
+        vector<int> hash(26, 0);
+        for(auto it: s){
+            hash[it-'a']++;
         }
-        else{
-            return false;
+
+        for(auto it: t){
+            hash[it-'a']--;
         }
+
+        for(auto it: hash){
+            if(it != 0) return false;
+        }
+
+        return true;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
     }
 };
