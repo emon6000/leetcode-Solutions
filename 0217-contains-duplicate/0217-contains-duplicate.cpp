4d062946938c274900263ebdc3833708d8484af2
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-01-14 23:30:27 +0600

    Time: 20 ms (90.45%), Space: 70.9 MB (88.54%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int>st;
-        int l=nums.size();
-        for(int i=0;i<l;i++){
-            st.insert(nums[i]);
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
         }
-        if(st.size()==l){
-            return false;
-        }
-        else{
-            return true;
-        }
+        return false;
+        // set<int>st;
+        // int l=nums.size();
+        // for(int i=0;i<l;i++){
+        //     st.insert(nums[i]);
+        // }
+        // if(st.size()==l){
+        //     return false;
+        // }
+        // else{
+        //     return true;
+        // }
     }
 };
