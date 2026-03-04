1ac8ccca08ddef2abc144f06d587423a65ae99a9
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-03-05 03:42:48 +0600

    Time: 6 ms (8.35%), Space: 11.3 MB (9.73%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,12 +1,24 @@
+// class Solution {
+// public:
+//     int rangeBitwiseAnd(int l, int r) {
+//         int shifts = 0;
+//         while (l < r) {
+//             l >>= 1;
+//             r >>= 1;
+//             shifts++;
+//         }
+//         return l << shifts;
+//     }
+// };
+
+// Brian Kernighan's Algorithm (Clearing the lowest set bit)
+
 class Solution {
 public:
     int rangeBitwiseAnd(int l, int r) {
-        int shifts = 0;
-        while (l < r) {
-            l >>= 1;
-            r >>= 1;
-            shifts++;
+        while (r > l) {
+            r &= (r - 1);
         }
-        return l << shifts;
+        return r;
     }
 };
