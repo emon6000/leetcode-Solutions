e569fdcc93ee16a028f10b3a0a8427dfff82d075
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-03-09 01:09:05 +0600

    Time: 4 ms (66.84%), Space: 23.9 MB (59.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
-        
+        if (intervals.empty())
+            return {};
         sort(intervals.begin(), intervals.end());
-        
-        int index = 0;
-        
+        int idx = 0;
+        vector<vector<int>>ans;
         for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] <= intervals[index][1]) {
-                intervals[index][1] = max(intervals[index][1], intervals[i][1]);
-            } 
-            else {
-                index++;
-                intervals[index] = intervals[i];
+            if (intervals[i][0] <= intervals[idx][1]) {
+                intervals[idx][1] = max(intervals[idx][1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[idx]);
+                idx=i;
             }
         }
-        
-        intervals.resize(index + 1);
-        return intervals;
+        ans.push_back(intervals[idx]);
+        return ans;
     }
 };
