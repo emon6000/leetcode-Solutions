6b8db29fc4ae40dfc21993753ad969bf8ba5d997
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-05-11 21:10:21 +0600

    Time: 0 ms (100%), Space: 15.2 MB (68.87%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> res;
-        for (int i = 0; i < n; i++) {
-            vector<int> ans;
-            if (nums[i] >= 10) {
-                int num = nums[i];
-                int x = num % 10;
-                ans.push_back(x);
-                num /= 10;
-                while (num) {
-                    ans.push_back(num % 10);
-                    num /= 10;
-                }
-            } else {
-                ans.push_back(nums[i]);
+        vector<int>ans;
+        for(auto &it : nums){
+            string s = to_string(it);
+            for(auto &c : s){
+                ans.push_back(c-'0');
             }
-            reverse(ans.begin(),ans.end());
-            res.insert(res.end(), ans.begin(), ans.end());
         }
-        return res;
+        return ans;
     }
 };
