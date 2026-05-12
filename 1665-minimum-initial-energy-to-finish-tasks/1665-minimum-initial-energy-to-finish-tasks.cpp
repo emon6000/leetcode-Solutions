7c047961809fb5ebdd4a0ddd5115ae30eaa92fcb
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-05-12 16:05:02 +0600

    Time: 32 ms (80.58%), Space: 97.5 MB (39.81%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,32 +1,57 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
-        int n = tasks.size();
-        vector<pair<int, int>> vp;
-        for (int i = 0; i < n; i++) {
-            int x = tasks[i][0];
-            int y = tasks[i][1];
-            int diff = y - x;
-            vp.push_back({diff, i});
-        }
-        sort(vp.rbegin(), vp.rend());
-        for (int i = 0; i < n; i++) {
-            cout << vp[i].first << " " << vp[i].second << endl;
-        }
-        int ans = 0, rem = 0;
-        for (int i = 0; i < n; i++) {
-            int y = vp[i].second;
-
-            int t = tasks[y][1];
-            int t2 = tasks[y][0];
-            cout << t << " " << t2 << endl;
-            if (rem < t) {
-                ans += (t - rem);
-                rem = t - t2;
+        sort(tasks.begin(), tasks.end(), [](const vector<int>& a, const vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+        
+        int ans = 0;
+        int rem = 0;
+        
+        for (const auto& task : tasks) {
+            int t = task[0];
+            int t2 = task[1];
+            
+            if (rem < t2) {
+                ans += (t2 - rem);
+                rem = t2 - t;
             } else {
-                rem = rem - t2;
+                rem = rem - t;
             }
         }
+        
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int minimumEffort(vector<vector<int>>& tasks) {
+//         int n = tasks.size();
+//         vector<pair<int, int>> vp;
+//         for (int i = 0; i < n; i++) {
+//             int x = tasks[i][0];
+//             int y = tasks[i][1];
+//             int diff = y - x;
+//             vp.push_back({diff, i});
+//         }
+//         sort(vp.rbegin(), vp.rend());
+//         for (int i = 0; i < n; i++) {
+//             cout << vp[i].first << " " << vp[i].second << endl;
+//         }
+//         int ans = 0, rem = 0;
+//         for (int i = 0; i < n; i++) {
+//             int y = vp[i].second;
+
+//             int t = tasks[y][1];
+//             int t2 = tasks[y][0];
+//             cout << t << " " << t2 << endl;
+//             if (rem < t) {
+//                 ans += (t - rem);
+//                 rem = t - t2;
+//             } else {
+//                 rem = rem - t2;
+//             }
+//         }
+//         return ans;
+//     }
+// };
