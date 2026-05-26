00892bd5ce606478b9cd18c172f5b795aba22022
Abdullah Al Mahmud Emon <121499858+emon6000@users.noreply.github.com>
2026-05-26 21:33:18 +0600

    Time: 0 ms (100%), Space: 7.9 MB (48.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int num) {
-        if (num > 0 && (num & (num - 1)) == 0) {
+        if(num == 0){
+            return false;
+        }
+        if (num == 1) {
             return true;
+        }
+        if (num % 2 == 0) {
+            return isPowerOfTwo(num / 2);
         }
         return false;
     }
